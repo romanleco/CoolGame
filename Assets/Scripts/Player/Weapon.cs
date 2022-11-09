034559b9c496ea86bcaa2343fb1c2a72c2b5be0b
romanleco <romangregori8@gmail.com>
2022-11-09 18:08:31 -0300

    Weapon Sprites

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -15,11 +15,17 @@ public class Weapon : MonoBehaviour
     [SerializeField] private float _fireRate = 0.1f;
     private Vector2 _localPosition;
     [SerializeField] private float _xOffset = 0.3f;
+    [SerializeField] private float _yOffset = -0.108f;
+    private float _startingLocalScaleX = 1;
+    private float _startingLocalScaleY = 1;
 
     void Start()
     {
         _fireTimer = new WaitForSeconds(_fireRate);
-        _localPosition.y = -0.108f;
+        _localPosition.y = _yOffset;
+
+        _startingLocalScaleX = transform.localScale.x;
+        _startingLocalScaleY = transform.localScale.y;
     }
 
     void Update() 
@@ -71,12 +77,12 @@ public class Weapon : MonoBehaviour
         Vector3 difference = transform.position - _mousePos;
         if(difference.x > 0.35f)
         {
-            transform.localScale = new Vector2(1, -1);
+            transform.localScale = new Vector2(_startingLocalScaleX, -_startingLocalScaleY);
             _localPosition.x = -_xOffset;
         }
         else if(difference.x < -0.35f)
         {
-            transform.localScale = new Vector2(1, 1);
+            transform.localScale = new Vector2(_startingLocalScaleX, _startingLocalScaleY);
             _localPosition.x = _xOffset;
         }
         transform.localPosition = _localPosition;
